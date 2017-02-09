4443b31becc2a56ed6119c55fcf9038d48fe39f2
extiser <1sappho@gmail.com>
2017-02-09 13:44:51 +0500

    full

--- builds/static/js/main.js
+++ builds/static/js/main.js
@@ -1,4 +1,29 @@
 jQuery(document).ready(function ($) {
+    $('#advantages-list').slick({
+        accessibility: false,
+        autoplay: false,
+        autoplaySpeed: 3000,
+        speed: 1000,
+        infinite: true,
+        arrows: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        centerPadding: 0,
+        pauseOnHover: false,
+        prevArrow: '<span class="slick-prev slick-nav slick-nav--prev" aria-label="previous"></span>',
+        nextArrow: '<span class="slick-next slick-nav slick-nav--next" aria-label="next"></span>',
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                arrows: true,
+                centerMode: true,
+                slidesToShow: 1
+            }
+        }]
+    });
+});
+
+jQuery(document).ready(function ($) {
     $('#articles-slide').slick({
         accessibility: false,
         autoplay: false,
@@ -22,31 +47,6 @@ jQuery(document).ready(function ($) {
         },
         {
             breakpoint: 640,
-            settings: {
-                arrows: true,
-                centerMode: true,
-                slidesToShow: 1
-            }
-        }]
-    });
-});
-
-jQuery(document).ready(function ($) {
-    $('#advantages-list').slick({
-        accessibility: false,
-        autoplay: false,
-        autoplaySpeed: 3000,
-        speed: 1000,
-        infinite: true,
-        arrows: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        centerPadding: 0,
-        pauseOnHover: false,
-        prevArrow: '<span class="slick-prev slick-nav slick-nav--prev" aria-label="previous"></span>',
-        nextArrow: '<span class="slick-next slick-nav slick-nav--next" aria-label="next"></span>',
-        responsive: [{
-            breakpoint: 768,
             settings: {
                 arrows: true,
                 centerMode: true,
